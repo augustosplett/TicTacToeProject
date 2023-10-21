736a79bb8d41b2600e33c5a1e5e94bef5a3de29a
feeoliveira-oficial <acmenergy@hotmail.com>
2023-10-21 14:50:04 -0400

    done all the features

--- src/Main.java
+++ src/Main.java
@@ -1,100 +1,14 @@
-import Block.Block;
-import Block.State.Blank;
-import Block.State.SimbolX;
+import java.util.Random;
 import java.util.Scanner;
-
-//TODO 1:
-//Implement/improve the function handleChoice to mark X or O according to the turn
-
-//TODO 2:
-//Implement the function to validate if some of the users won the game after every turn (change KEEP_PLAYING = false)
-
-//TODO 3:
-//Implement a function to get the name of the player one and Player 2
-
-//TODO 4:
-//Implement a function to show the winner right after he/she won the match
-
-//TODO 5:
-//Implement a factory to remove the static methods from the main file
-
-//TODO 6:
-//Implement/improve the function that receive the user input to accept "A1" and/or "1A"
-
-
 public class Main {
-    public static boolean KEEP_PLAYING = true;
-    public static void main(String[] args) {
-        createNewGame();
-        while(KEEP_PLAYING){
-            displayBoard();
-            captureChoice();
-        }
-
-    }
-
-    public static Block[][] BLOCKS = new Block[3][3];
-    public static void displayBoard(){
-        System.out.println("Hello world!");
-        System.out.println("    +---+---+---+");
-        System.out.println("    | A | B | C |");
-        System.out.println("+---+---+---+---+");
-        System.out.println("| 1 | " + BLOCKS[0][0].getState().display()
-                + " | " + BLOCKS[0][1].getState().display()
-                + " | " + BLOCKS[0][2].getState().display() + " |");
-        System.out.println("+---+---+---+---+");
-        System.out.println("| 2 | " + BLOCKS[1][0].getState().display()
-                + " | " + BLOCKS[1][1].getState().display()
-                + " | " + BLOCKS[1][2].getState().display() + " |");
-        System.out.println("+---+---+---+---+");
-        System.out.println("| 3 | " + BLOCKS[2][0].getState().display()
-                + " | " + BLOCKS[2][1].getState().display()
-                + " | " + BLOCKS[2][2].getState().display() + " |");
-        System.out.println("+---+---+---+---+");
-    }
-
-    public static void createNewGame(){
-        for(byte i = 0; i < 3; i++){
-            for(byte j = 0; j < 3; j ++){
-                BLOCKS[i][j] = new Block(Blank.instance());
-            }
-        }
-    }
-    public static void captureChoice(){
-        var scanner = new Scanner(System.in);
-        System.out.print("Insert the cell you want: ");
-        var choice = scanner.next();
-        handleChoice(choice);
-    }
-    public static void handleChoice(String choice){
-        switch (choice.toUpperCase()){
-            case "A1":
-                BLOCKS[0][0].setState(SimbolX.instance());
-                break;
-            case "A2":
-                BLOCKS[1][0].setState(SimbolX.instance());
-                break;
-            case "A3":
-                BLOCKS[2][0].setState(SimbolX.instance());
-                break;
-            case "B1":
-                BLOCKS[0][1].setState(SimbolX.instance());
-                break;
-            case "B2":
-                BLOCKS[1][1].setState(SimbolX.instance());
-                break;
-            case "B3":
-                BLOCKS[2][1].setState(SimbolX.instance());
-                break;
-            case "C1":
-                BLOCKS[0][2].setState(SimbolX.instance());
-                break;
-            case "C2":
-                BLOCKS[1][2].setState(SimbolX.instance());
-                break;
-            case "C3":
-                BLOCKS[2][2].setState(SimbolX.instance());
-                break;
+    public static void main(String[] args)
+    {
+        boolean KEEP_PLAYING = true;
+        while (true) {
+            new createNewGame();
+            createNewGame.displayBoard();
+            createNewGame.playGame();
+            createNewGame.showResults();
         }
     }
 }
---
+++ src/createNewGame.java
@@ -0,0 +1,92 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class createNewGame
+{
+        public static char[][] BLOCKS = new char[3][3];
+        public static int[] SCORES = new int[2]; // Index 0 for player X, Index 1 for player O
+        public static char CURRENT_PLAYER;
+
+        public createNewGame() {
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    BLOCKS[i][j] = ' ';
+                }
+            }
+            CURRENT_PLAYER = new Random().nextBoolean() ? 'X' : 'O'; // Randomly choosing the first player
+        }
+
+        public static void displayBoard () {
+            System.out.println("    A   B   C ");
+            System.out.println("  +---+---+---+");
+            for (int i = 0; i < 3; i++) {
+                System.out.print(i + 1 + " ");
+                for (int j = 0; j < 3; j++) {
+                    System.out.print("| " + BLOCKS[i][j] + " ");
+                }
+                System.out.println("|");
+                System.out.println("  +---+---+---+");
+            }
+        }
+
+        public static void playGame () {
+            Scanner scanner = new Scanner(System.in);
+            boolean gameWon = false;
+            int totalMoves = 0;
+
+            while (!gameWon && totalMoves < 9) {
+                System.out.print("Player " + CURRENT_PLAYER + ", enter your move (e.g., A1): ");
+                String move = scanner.next();
+                if (handleChoice(move)) {
+                    displayBoard();
+                    gameWon = checkVictory();
+                    if (!gameWon) {
+                        totalMoves++;
+                        CURRENT_PLAYER = (CURRENT_PLAYER == 'X') ? 'O' : 'X';
+                    }
+                } else {
+                    System.out.println("Invalid move. Try again.");
+                }
+            }
+        }
+
+        public static boolean handleChoice (String choice){
+            int row = Integer.parseInt(choice.substring(1)) - 1;
+            int col = switch (choice.charAt(0)) {
+                case 'A', 'a' -> 0;
+                case 'B', 'b' -> 1;
+                case 'C', 'c' -> 2;
+                default -> -1;
+            };
+            if (row >= 0 && row < 3 && col >= 0 && BLOCKS[row][col] == ' ') {
+                BLOCKS[row][col] = CURRENT_PLAYER;
+                return true;
+            }
+            return false;
+        }
+
+        public static boolean checkVictory () {
+            for (int i = 0; i < 3; i++) {
+                if ((BLOCKS[i][0] == CURRENT_PLAYER && BLOCKS[i][1] == CURRENT_PLAYER && BLOCKS[i][2] == CURRENT_PLAYER) ||
+                        (BLOCKS[0][i] == CURRENT_PLAYER && BLOCKS[1][i] == CURRENT_PLAYER && BLOCKS[2][i] == CURRENT_PLAYER)) {
+                    return true;
+                }
+            }
+            return (BLOCKS[0][0] == CURRENT_PLAYER && BLOCKS[1][1] == CURRENT_PLAYER && BLOCKS[2][2] == CURRENT_PLAYER) ||
+                    (BLOCKS[0][2] == CURRENT_PLAYER && BLOCKS[1][1] == CURRENT_PLAYER && BLOCKS[2][0] == CURRENT_PLAYER);
+        }
+
+        public static void showResults () {
+            if (checkVictory()) {
+                System.out.println("Player " + CURRENT_PLAYER + " wins!");
+                if (CURRENT_PLAYER == 'X') SCORES[0]++;
+                else SCORES[1]++;
+            } else {
+                System.out.println("It's a draw!");
+            }
+            System.out.println("Score: X - " + SCORES[0] + ", O - " + SCORES[1]);
+            System.out.print("Do you want to play again? (y/n): ");
+            Scanner scanner = new Scanner(System.in);
+            boolean KEEP_PLAYING = scanner.next().charAt(0) == 'y';
+        }
+    }
